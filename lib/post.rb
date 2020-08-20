d3acb5da75421d53b3a3491fb0bccd65371302c7
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 20:13:22 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+  attr_accessor :title, :author
+
+  def assign(title, author)
+    @title = title
+    @author = author
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+  attr_accessor :title, :artist
+
+  def assign(title, artist)
+    @title = title
+    @artist = artist
+  end
+end
